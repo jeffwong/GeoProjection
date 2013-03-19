fd860e86a84f79ba577be47aff5f1a2b9be3c521
jeffwong <jeff.ct.wong@gmail.com>
2013-03-19 16:43:31 -0700

    geolookup only returned a lookup table, which still had to be left joined with inputs

--- R/geoprojection.R
+++ R/geoprojection.R
@@ -16,6 +16,36 @@ projectLatLon = function(latlon) {
     GeoLookup[matching.indices,]
 }
 
+#' GeoProjection
+#'
+#' Append geographic data based on one of zip, city, or state
+#'
+#' @param zip
+#' @param city
+#' @param state
+#' @return a data table with columns zip, lat, lon, city, state, county
+#' @examples
+#'  projectGeo(zip = c(94109, 94306))
+#' @export
+projectGeo = function(zip, city, state) {
+    if (!missing(zip)) {
+        zip.dt = data.table(Zip = zip)
+        geoprojection = GeoLookup[Zip %in% zip]
+        geoprojection = merge(zip.dt, geoprojection, all.x = T, by = "Zip")
+    }
+    else if (!missing(city)) {
+        city.dt = data.table(City = city)
+        geoprojection = GeoLookup[City %in% toupper(city)]
+        geoprojection = merge(city.dt, geoprojection, all.x = T, by = "City")
+    }
+    else if (!missing(state)) {
+        state.dt = data.table(State = state)
+        geoprojection = GeoLookup[State %in% toupper(state)]
+        geoprojection = merge(state.dt, geoprojection, all.x = T, by = "State")
+    }
+    return (geoprojection[,-1,with=F])
+}
+
 #' Geo Lookup
 #'
 #' Find a geo record given one of either zip, city, state, or county.
